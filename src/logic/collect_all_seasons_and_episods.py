9575c277b5485459f633156621b70a1649a96fbf
ZKWolf <zkwolf@zkwolf.stream>
2022-06-24 01:04:33 +0200

    added counter for seasons and episodes in seasons.

--- src/logic/collect_all_seasons_and_episods.py
+++ src/logic/collect_all_seasons_and_episods.py
@@ -14,13 +14,14 @@ def get_season(anime_name):
     return counter_seasons - 1
 
 
-def get_episods(season_count, anime_name):
-    anime_url = "https://aniworld.to/anime/stream/{}/".format(anime_name)
-    counter_episods = 1
+def get_episodes(season_count, anime_name):
+    anime_url = "https://aniworld.to/anime/stream/{}/staffel-{}/".format(anime_name, season_count)
+    episode_count = 1
     html_page = urllib.request.urlopen(anime_url)
     soup = BeautifulSoup(html_page, features="html.parser")
     for link in soup.findAll('a'):
-        seasons = str(link.get("href"))
-        if "/anime/stream/{}/staffel-{}/".format(anime_name, counter_seasons) in seasons:
-            counter_seasons = counter_seasons + 1
-    return counter_seasons - 1
+        episode = str(link.get("href"))
+        if "/anime/stream/{}/staffel-{}/episode-{}".format(anime_name, season_count, episode_count) in episode:
+            episode_count = episode_count + 1
+    print(episode_count - 1)
+    return episode_count - 1
