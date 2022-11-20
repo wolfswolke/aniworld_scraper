6d0633456e711ae4c36232d96eaf920d33559717
ZKWolf <zkwolf@zkwolf.com>
2022-11-20 23:25:08 +0100

    Reformated downloader.py IDK why its SLOW AF but ill fix that later...

--- src/logic/downloader.py
+++ src/logic/downloader.py
@@ -1,13 +1,10 @@
 from tqdm import tqdm
 import requests
 
-from logic.search_for_links import vidoza_to_cache
-
 
 def downloader(url, file_name):
-    cache = vidoza_to_cache(url)
-    response = requests.get(cache, stream=True)
-    total_size_in_bytes= int(response.headers.get('content-length', 0))
+    response = requests.get(url, stream=True, timeout=10)
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
     block_size = 1024
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
     with open(file_name, 'wb') as file:
