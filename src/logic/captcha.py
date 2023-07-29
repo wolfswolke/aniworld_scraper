a259c2cc31ce04dc4e00e5821780c45e6f5dba90
ZKWolf <zkwolf@zkwolf.com>
2023-07-29 16:14:42 +0200

    Fixed driver missing issue.

--- src/logic/captcha.py
+++ src/logic/captcha.py
@@ -6,7 +6,6 @@
 # ------------------------------------------------------- #
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from threading import Thread
 
@@ -37,10 +36,9 @@ def open_captcha_window(full_url):
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     if os.path.exists(path_to_ublock):
         options.add_argument('load-extension=' + path_to_ublock)
-    # if you encounter this error ___selenium.common.exceptions.WebDriverException: Message: unknown error: cannot find Chrome binary____
-    # set the next line to the specific Google Chrome binary on your system
-    # options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(full_url)
 
     wait = WebDriverWait(driver, 100, 0.3)
