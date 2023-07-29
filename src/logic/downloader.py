2c7126914a1b4eb535d69a2959610585616490db
speedyconzales <chriskloeffel@gmail.com>
2023-07-29 20:12:16 +0200

    found a logic error -> fixed it

--- src/logic/downloader.py
+++ src/logic/downloader.py
@@ -9,26 +9,26 @@ MODULE_LOGGER_HEAD = "downloader.py -> "
 
 
 def download(link, file_name):
-    downloading = True
     retry_count = 0
-    while downloading:
+    while True:
         logger.debug(MODULE_LOGGER_HEAD + "Entered download with these vars: Link: {}, File_Name: {}".format(link, file_name))
         r = requests.get(link, stream=True)
         with open(file_name, 'wb') as f:
             for chunk in r.iter_content(1024):
                 if chunk:
                     f.write(chunk)
-        if retry_count == 1:
-            logger.error(MODULE_LOGGER_HEAD + "Server error. Could not download {}. Please manly download it later.".format(file_name))
-            downloading = False
         if path.getsize(file_name) != 0:
-            downloading = False
+            logger.info(MODULE_LOGGER_HEAD + "Finished download of {}.".format(file_name))
+            break
+        elif retry_count == 1:
+            logger.error(MODULE_LOGGER_HEAD + "Server error. Could not download {}. Please manly download it later.".format(file_name))
+            break
         else:
-            logger.error(MODULE_LOGGER_HEAD + "Download did not complete! File {} will be retryd in a few seconds.".format(file_name))
+            logger.info(MODULE_LOGGER_HEAD + "Download did not complete! File {} will be retryd in a few seconds.".format(file_name))
             logger.debug(MODULE_LOGGER_HEAD + "URL: {}, filename {}".format(link, file_name))
             time.sleep(20)
             retry_count = 1
-    logger.info(MODULE_LOGGER_HEAD + "Finished download of {}.".format(file_name))
+    
 
 
 def create_new_download_thread(url, file_name):
