902dfd739f98df43f33e33f106d6fb2629412283
lkshrsch <lkshrsch2001@outlook.com>
2023-11-30 20:52:49 +0100

    Rename Anime-Name-Goes-Here to Name-Goes-Here

--- src/constants.py
+++ src/constants.py
@@ -25,14 +25,16 @@ APP_VERSION = "v01-10-00"
 #                   global variables
 # ------------------------------------------------------- #
 type_of_media = parse_cli_arguments("anime", 1)  # choose 'serie' or 'anime'
-name = parse_cli_arguments("Anime-Name-Goes-Here", 2)
+name = parse_cli_arguments("Name-Goes-Here", 2)
 language = parse_cli_arguments("Deutsch", 3)
 season_override = parse_cli_arguments(0, 4)  # 0 = no override. 1 = season 1. etc...
 episode_override = 0  # 0 = no override. 1 = episode 1. etc...
 ddos_protection_calc = 5
 ddos_wait_timer = 180  # in seconds
 output_path = name
-site_url = {"serie": "https://s.to",  # maybe you need another dns to be able to use this site
-                "anime": "https://aniworld.to"}
+site_url = {
+    "serie": "https://s.to",  # maybe you need another dns to be able to use this site
+    "anime": "https://aniworld.to"
+}
 
 url = "{}/{}/stream/{}/".format(site_url[type_of_media], type_of_media, name)
