0de62f12c21b9243a355cc4353a185abb76d5ea2
ZKWolf <zkwolf@zkwolf.com>
2023-12-25 21:58:39 +0100

    Updated run.sh to  have the movie parm for #30

--- run.sh
+++ run.sh
@@ -6,8 +6,9 @@ NAME="Name-Goes-Here"
 LANGUAGE="Deutsch" # most common: ["Deutsch","Ger-Sub","English"]
 SEASON=0 # 0 means all seasons otherwise specify the season you want
 NUM_RUNS=1
+MOVIE=False
 
 for ((i=1; i<=NUM_RUNS; i++))
 do
-    python3 "$SCRIPT_PATH" "$TYPE" "$NAME" "$LANGUAGE" "$SEASON"
+    python3 "$SCRIPT_PATH" "$TYPE" "$NAME" "$LANGUAGE" "$MOVIE" "$SEASON"
 done
