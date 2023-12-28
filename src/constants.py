434d3a4ca8f2afc96d9af0653c24a2f7d323543d
ZKWolf <zkwolf@zkwolf.com>
2023-12-28 21:10:54 +0100

    #30 implemented Bramdarl fix for movies.

--- src/constants.py
+++ src/constants.py
@@ -13,6 +13,7 @@ def parse_cli_arguments(default: str | int, position: int) -> str | int:
             cli_argument: int = int(cli_argument)
         return cli_argument
     except IndexError:
+        logger.debug(f"no cli argument detected on position:{position}. Using default value:{default}")
         return default
 
 
@@ -27,7 +28,7 @@ APP_VERSION = "v01-10-00"
 type_of_media = parse_cli_arguments("anime", 1)  # choose 'serie' or 'anime'
 name = parse_cli_arguments("Name-Goes-Here", 2)
 language = parse_cli_arguments("Deutsch", 3)
-movie = parse_cli_arguments(False, 4)  # True = movie. False = episode
+movie = (parse_cli_arguments(False, 4).lower() == 'true')  # True = movie. False = episode  # True = movie. False = episode
 season_override = parse_cli_arguments(0, 5)  # 0 = no override. 1 = season 1. etc...
 episode_override = 0  # 0 = no override. 1 = episode 1. etc...
 ddos_protection_calc = 5
