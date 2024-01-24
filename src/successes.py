7efef06a4e0f1d5d3c2a831d8e8ac46ae4daa726
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-01-24 15:54:17 +0100

    Add failure and success log handling
    Introduced new files, 'failures.py' and 'successes.py', to handle and log failures and successes respectively. Modified 'custom_logging.py' to support this functionality, including the addition of a custom formatter and a new log level for successes.

---
+++ src/successes.py
@@ -0,0 +1,25 @@
+from src.custom_logging import setup_logger
+
+logger = setup_logger(__name__)
+filename = "src/successes.txt"
+open(filename, "w").close()
+successes = []
+
+
+def append_success(success):
+    logger.debug(f"Appended success {success}")
+    writer = open(filename, "a")
+    writer.write(success)
+    writer.write("\n")
+    writer.close()
+    successes.append(success)
+
+
+def write_succs():
+    logger.debug("Writing successes")
+    successes.sort()
+    writer = open(filename, "w")
+    for success in successes:
+        writer.write(success)
+        writer.write("\n")
+    writer.close()
