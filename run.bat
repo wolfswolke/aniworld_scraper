509f8269a5d3b1d460c945871364c55f9e0cdb12
ZKWolf <34097612+wolfswolke@users.noreply.github.com>
2024-04-30 20:32:34 +0200

    Started working on translations GERMAN and ENGLISH for batch. Code soon

--- run.bat
+++ run.bat
@@ -1,97 +1,126 @@
 @echo off
-
 cls
+echo Select your Display Language:
+echo Waehle deine anzeige Sprache:
+echo.
+echo 1. English
+echo 2. Deutsch
+echo.
+set /p LANG=Language:
+if %LANG%==1 goto eng
+if %LANG%==2 goto ger
 
-echo Welcome to AniWorldScraper!
+:eng
+set hello_header=Welcome to AniWorldScraper!
+set choose_media=Please choose the media type you want to download:
+set media_type_anime=1. Anime (Aniworld.to)
+set media_type_series=2. Series (s.to)
+set media_name=Please enter the name of the media you want to download:
+set media_desc=Use the exact name as on the website. (angels-of-death)
+set media_lang=Please choose the language of the media you want to download:
+set media_choice1=1. German
+set media_choice2=2. German Subtitles
+set media_choice3=3. English
+set download_mode=Please choose the download mode:
+set download_mode_movies=1. Only Movies
+set download_mode_series=2. Only Series
+set download_mode_all=3. All Content
+set season_header=Please enter the season you want to download:
+set season_desc=Enter 0 for all seasons.
+set provider_header=Please choose the provider you want to download from:
+set provider_desc=Default value is VOE
+set start_scraper=Starting scraper now...
+set finish_scraper=Done!
+goto start
+
+:ger
+set hello_header=Willkommen bei AniWorldScraper!
+set choose_media=Bitte waehle den Medientyp den du herunterladen moechtest:
+set media_type_anime=1. Anime (Aniworld.to)
+set media_type_series=2. Serie (s.to)
+set media_name=Bitte gebe den Namen des Mediums ein, das du herunterladen moechtest:
+set media_desc=Verwende den genauen Namen wie auf der Website. (angels-of-death)
+set media_lang=Bitte waehle die Sprache des Mediums, das du herunterladen moechtest:
+set media_choice1=1. Deutsch
+set media_choice2=2. Mit deutschen Untertiteln
+set media_choice3=3. Englisch
+set download_mode=Bitte waehle den Download-Modus:
+set download_mode_movies=1. Nur Filme
+set download_mode_series=2. Nur Serien
+set download_mode_all=3. Alle Inhalte
+set season_header=Bitte geben Sie die Staffel ein, die Sie herunterladen moechten:
+set season_desc=Geben Sie 0 fuer alle Staffeln ein.
+set provider_header=Bitte waehlen Sie den Anbieter aus, von dem Sie herunterladen moechten:
+set provider_desc=Standard wert ist VOE
+set start_scraper=Scraper wird jetzt gestartet...
+set finish_scraper=Fertig!
+goto start
+
+:start
+echo %hello_header%
 echo.
-echo Please choose the media type you want to download:
+echo %choose_media%
 echo.
-echo 1. Anime (Aniworld.to)
-echo 2. Series (s.to)
+echo %media_type_anime%
+echo %media_type_series%
 echo.
 set /p TYPE=Type:
-
 if %TYPE%==1 set TYPE=anime
 if %TYPE%==2 set TYPE=serie
 cls
-
 echo.
-echo Please enter the name of the media you want to download:
-echo Use the exact name as on the website. (angels-of-death)
+echo %media_name%
+echo %media_desc%
 echo.
 set /p NAME=Name:
 cls
-
 echo.
-echo Please choose the language of the media you want to download:
+echo %media_lang%
 echo.
-echo 1. Deutsch
-echo 2. Ger-Sub
-echo 3. English
+echo %media_choice1%
+echo %media_choice2%
+echo %media_choice3%
 echo.
 set /p LANGUAGUE=Language:
-
 if %LANGUAGUE%==1 set LANGUAGUE=Deutsch
 if %LANGUAGUE%==2 set LANGUAGUE=Ger-Sub
 if %LANGUAGUE%==3 set LANGUAGUE=English
 cls
-
 echo.
-echo Please choose the download mode:
+echo %download_mode%
 echo.
-echo 1. Only Movies
-echo 2. Only Series
-echo 3. All Content
+echo %download_mode_movies%
+echo %download_mode_series%
+echo %download_mode_all%
 echo.
 set /p DLMODE=dlMode:
-
 if %DLMODE%==1 set DLMODE=Movies
 if %DLMODE%==2 set DLMODE=Series
 if %DLMODE%==3 set DLMODE=All
 cls
-
 echo.
-echo Please enter the season you want to download:
-echo Enter 0 for all seasons.
+echo %season_header%
+echo %season_desc%
 echo.
 set /p SEASON=Season:
 cls
-
 echo.
-echo Please choose the provider you want to download from:
-echo Default is VOE.
+echo %provider_header%
+echo %provider_desc%
 echo.
 echo 1. VOE
 echo 2. Vidoza
 echo 3. Streamtape
 echo.
 set /p PROVIDER=Provider:
-
 if %PROVIDER%==1 set PROVIDER=VOE
 if %PROVIDER%==2 set PROVIDER=Vidoza
 if %PROVIDER%==3 set PROVIDER=Streamtape
 cls
-
 echo.
-echo Starting scraper now...
+echo %start_scraper%
 
-REM Set the values from command line arguments
 set SCRIPT_PATH=main.py
-REM set TYPE=anime
-REM set NAME=Name-Goes-Here
-REM set LANGUAGUE=Deutsch
-REM set DLMODE=Series
-REM set SEASON=0
-REM set PROVIDER=VOE
-
-rem Script path = File to run
-rem Type = anime or serie
-rem Name = Name of the anime or series
-rem Language = Language of the anime or series most common: ["Deutsch","Ger-Sub","English"]
-rem dlMode = Choose your Content ["Movies", "Series", "All"]
-rem Season = 0 means all seasons otherwise specify the season you want
-rem Provider = Choose your Provider ["VOE", "Vidoza", "Streamtape"]
-
 python %SCRIPT_PATH% %TYPE% %NAME% %LANGUAGUE% %DLMODE% %SEASON% %PROVIDER%
-echo Done!
+echo %finish_scraper%
 exit
